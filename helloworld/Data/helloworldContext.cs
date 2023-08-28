1bc7314b44919f4487dd49e02f9ee83d87f0b93f
nawimnate <jnhatch3@gmail.com>
2023-08-28 18:05:39 -0400

    Add project files.

---
+++ helloworld/Data/helloworldContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace helloworld.Data
+{
+    public class helloworldContext : DbContext
+    {
+        public helloworldContext (DbContextOptions<helloworldContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
